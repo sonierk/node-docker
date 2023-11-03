433efa3a0ffd4da1940b3ff85276fc70fc996773
sonierk <sonierk@gmail.com>
2023-11-03 07:55:15 +0530

    Setup named vol for persistence vol for DB - configure mongoose connection from node to DB and do test

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
+
+mongoose.connect('mongodb://sanjeev:mypassword@mongo:27017/?authSource=admin')
+  .then(() => console.log('Connected to DB!'))
+  .catch((e)=>console.log(e));
+
+
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
-    res.send('<h2> All!!!</h2>')
+    res.send('<h2>Hi All!!!</h2>')
 })
 
 app.listen(port, ()=>console.log(`listening on port ${port}`))
